6f753c24527b15f31a0c688eb25e5cac711ccce5
Faraz Ahmed <masterdot707@gmail.com>
2022-12-03 05:58:41 +0500

    Created Footer.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import NewsLetter from "./components/NewsLetter";
 import Cards from "./components/Cards.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Analytics />
       <NewsLetter />
       <Cards />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto py-16 px-6 grid lg:grid-cols-3 text-gray-300">
+      <div>
+        <h1 className="w-full font-bold text-3xl text-[#00df98]">React</h1>
+        <p className="py-4 mr-3">
+          In publishing and graphic design, Lorem ipsum is a placeholder text
+          commonly used to demonstrate the visual form of a document or a
+          typeface without relying on meaningful content.{" "}
+        </p>
+        <div className="flex md:w-[75%] my-6 justify-between">
+          <FaFacebookSquare size={30} />
+          <FaTwitterSquare size={30} />
+          <FaGithubSquare size={30} />
+          <FaDribbbleSquare size={30} />
+          <FaInstagram size={30} />
+        </div>
+      </div>
+      <div className="lg:col-span-2 flex justify-between mt-6">
+        <div>
+          <h6 className="font-medium text-gray-400">Solutions</h6>
+          <ul>
+            <li className="py-2 text-sm">Analytics</li>
+            <li className="py-2 text-sm">Marketing</li>
+            <li className="py-2 text-sm">Commerce</li>
+            <li className="py-2 text-sm">Insights</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Support</h6>
+          <ul>
+            <li className="py-2 text-sm">Pricing</li>
+            <li className="py-2 text-sm">Documentation</li>
+            <li className="py-2 text-sm">Guides</li>
+            <li className="py-2 text-sm">API Status</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Company</h6>
+          <ul>
+            <li className="py-2 text-sm">About</li>
+            <li className="py-2 text-sm">Blog</li>
+            <li className="py-2 text-sm">Jobs</li>
+            <li className="py-2 text-sm">Press</li>
+            <li className="py-2 text-sm">Careers</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Legal</h6>
+          <ul>
+            <li className="py-2 text-sm">Claim</li>
+            <li className="py-2 text-sm">Policy</li>
+            <li className="py-2 text-sm">Terms</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
